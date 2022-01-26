58ebe54df35b9a9882db44d075ef763d863f7df1
CraigJensen <cmjensen82@gmail.com>
2022-01-26 13:26:37 -0600

    added path to index and notes.html

--- server.js
+++ server.js
@@ -1,7 +1,34 @@
 const express = require('express');
+const fs = require('fs');
+const path = require('path');
+const { notes } = require('./db/db');
 
 const app = express();
 app.use(express.static('public'));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/notes.html'));
+});
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.get('/api/notes', (req, res) => {
+  res.json(notes);
+});
+
+app.get('/api/notes/:id', (req, res) => {
+  res.json(notes);
+});
+
+// app.post('/api/notes', (req, res) => {
+//   console.log(req.body);
+//   res.json(req.body);
+// });
 
 app.listen(3001, () => {
   console.log(`API server now on port 3001!`);
