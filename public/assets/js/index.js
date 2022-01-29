beb3042f94192fe30c877eebef21f0b9331eee43
CraigJensen <cmjensen82@gmail.com>
2022-01-29 07:54:41 -0600

    started js file to display to html

---
+++ public/assets/js/index.js
@@ -0,0 +1,36 @@
+let noteTitle = document.querySelector('.note-title');
+let noteText = document.querySelector('.note-textarea');
+let saveNoteBtn = document.querySelector('.save-note');
+let newNoteBtn = document.querySelector('.new-note');
+let noteList = document.querySelectorAll('.list-container .list-group');
+
+const showData = (element) => (element.style.display = 'inline');
+
+const hideData = (element) => (element.style.display = 'none');
+
+let currentNote = {};
+
+const getNotes = () =>
+  fetch('/api/notes', {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+
+const saveNote = (note) =>
+  fetch('/api/notes', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(note),
+  });
+
+const deleteNote = (id) =>
+  fetch(`/api/notes/${id}`, {
+    method: 'DELETE',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
